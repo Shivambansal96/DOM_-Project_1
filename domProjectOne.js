133addef3ce160be28b6d1912e566cf91f84dce7
ShivamBansal96 <bansal.shivam1216@gmail.com>
2024-01-12 15:47:19 +0530

    DOM Project 1 done.

---
+++ domProjectOne.js
@@ -0,0 +1,19 @@
+let outerBox = document.getElementById("box");
+
+outerBox.innerHTML = "Hello, I'm a div!";
+
+outerBox.style.backgroundColor = "yellow";
+
+outerBox.style.margin = "20px";
+
+outerBox.style.padding = "10px";
+
+outerBox.style.fontSize = "18px";
+
+outerBox.style.color = "blue";
+
+outerBox.style.fontWeight = "bold";
+
+outerBox.style.height = "200px";
+
+outerBox.style.width = "300px";
